7d9abc7e1d427bc220ff1c4d0aa215c8fc88b4bd
mjnrock <mjnrock@umich.edu>
2018-05-29 21:51:14 -0400

    init

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = express();
 const PORT = 3005;
 
 app.listen(PORT, () => {
-  console.log(`Hangtime server is now listening on port: ${PORT}`);
+	console.log(`Hangtime server is now listening on port: ${PORT}`);
 });
